c26154dff9229aa2f23fe8ddeedec3157fc2eeb6
James Phelps <jamesph2012@outlook.com>
2025-11-08 13:58:17 +0000

    Edited startSettings() function alert()

--- script.js
+++ script.js
@@ -1303,31 +1303,7 @@ function startGame(){
 }
 
 function startSettings() {
-	// If already open, don’t open another
-	if (document.getElementById("settingsMenu")) return;
-
-	// Create menu container
-	const menu = document.createElement("div");
-	menu.id = "settingsMenu";
-	menu.innerHTML = `
-		<div id="settingsContent">
-			<div id="settingsTitle" class="title">SETTINGS</div>
-			<div class="settingsOption">Map Theme: <span class="value">Classic</span></div>
-			<div class="settingsOption">Trees: <span class="value">Normal</span></div>
-			<div class="settingsOption">Lighting: <span class="value">Day</span></div>
-			<div id="closeSettings" class="button">Close</div>
-		</div>
-	`;
-	document.body.appendChild(menu);
-
-	// Delay to trigger transition
-	setTimeout(() => menu.classList.add("show"), 10);
-
-	// Close on button click
-	document.getElementById("closeSettings").onclick = () => {
-		menu.classList.remove("show");
-		setTimeout(() => menu.remove(), 500);
-	};
+	alert("coming soon!")
 }
 
 
